a7d704cf2be2e36892320f2a53203431aa051886
Mustafa Madkour <madkour2790@gmail.com>
2020-06-24 02:02:04 +0200

    sign up form added

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './core/Home';
 import Users from './user/Users';
+import Signup from './user/Signup';
 
 const MainRouter = () => {
 	return (
-		<div>																																																																																																																																																																																																																																																																																																																																																																																																														
+		<div>
 			<Switch>
 				<Route exact path="/" component={Home} />
 				<Route path="/users" component={Users} />
+				<Route path="/signup" component={Signup} />
 			</Switch>
 		</div>
 	);
